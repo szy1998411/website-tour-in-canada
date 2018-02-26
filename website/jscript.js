467c06143d9f9653678f9482caf02b04f3d7b601
szy1998411 <sunzhenyu1998411@outlook.com>
2018-02-25 23:34:48 -0500

    Add files via upload

---
+++ website/jscript.js
@@ -0,0 +1,13 @@
+var modal = document.getElementById('myModal');
+
+var modalImg = document.getElementById("img01");
+function call(){
+    modal.style.display = "block";
+    modalImg.src = "img_fjords.jpg";
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+    modal.style.display = "none";
+}
