7d264ae33bd0759a0ff2495228732c32b6d5f8e4
szy1998411 <sunzhenyu1998411@outlook.com>
2018-02-28 01:32:46 -0500

    Add background blur

--- website/jscript.js
+++ website/jscript.js
@@ -1,15 +1,17 @@
 var modal = document.getElementById('myModal');
-
 var modalImg = document.getElementById("img01");
+var back1 = document.getElementById("back");
 function call(){
     modal.style.display = "block";
     modalImg.src = "img_fjords.jpg";
+    back1.classList.add("blur2");
 }
 
 var span = document.getElementsByClassName("close")[0];
 
 span.onclick = function() { 
     modal.style.display = "none";
+    back1.classList.remove("blur2");
 }
 
 function home(){
