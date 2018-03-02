40806c8430e05a7aa26559503c086751a647151a
Joe is cute <sunzhenyu1998411@outlook.com>
2018-03-02 15:06:08 -0500

    Modified links on pop up image

--- website/jscript.js
+++ website/jscript.js
@@ -1,9 +1,10 @@
 var modal = document.getElementById('myModal');
 var modalImg = document.getElementById("img01");
 var back1 = document.getElementById("back");
+var trt=document.getElementById("toronto");
 function call(){
     modal.style.display = "block";
-    modalImg.src = "img_fjords.jpg";
+    modalImg.src = "grey map.jpg";
     back1.classList.add("blur2");
 }
 
@@ -13,6 +14,7 @@ span.onclick = function() {
     modal.style.display = "none";
     back1.classList.remove("blur2");
 }
+
 
 function home(){
 	location.href = "travel in canada.html";
